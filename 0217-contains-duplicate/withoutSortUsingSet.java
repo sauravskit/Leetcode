7b7a2b74bd430d06df9536994c13a5d0cc7deb86
Saurav Kumar <150720426+sauravskit@users.noreply.github.com>
2024-05-23 01:07:32 +0530

    Update withoutSortUsingSet.java

--- 0217-contains-duplicate/withoutSortUsingSet.java
+++ 0217-contains-duplicate/withoutSortUsingSet.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set=new HashSet<>();
         for(int temp:nums){
-            if(set.contains(temp)) return true;set.put(temp);
+            if(set.contains(temp)) return true;set.add(temp);
         }
         return false;
     }
