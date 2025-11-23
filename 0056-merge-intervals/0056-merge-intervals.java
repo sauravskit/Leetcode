4ae99d9b987eed8bebc018ee155d996ffc8acd6d
Saurav Kumar <150720426+sauravskit@users.noreply.github.com>
2025-11-23 23:10:48 +0530

    Time: 8 ms (87.25%), Space: 49 MB (17.45%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,24 @@
 class Solution {
-	public int[][] merge(int[][] intervals) {
-		if (intervals.length <= 1)
-			return intervals;
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[]> result = new ArrayList<>();
+        for(int[] interval :intervals){
+            /*here we are comparing lets say [1,3],[8,10] -> where 1,3 has been added to the new list(result)
+            and 8,10 is the new array coming from interval if result last node is less than interval first node
+            then that is a new interval i.e 3<8 so [1,3],[8,10] 
 
-		// Sort by ascending starting point
-		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-
-		List<int[]> result = new ArrayList<>();
-		int[] newInterval = intervals[0];
-		result.add(newInterval);
-		for (int[] interval : intervals) {
-			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
-				newInterval[1] = Math.max(newInterval[1], interval[1]);
-			else {                             // Disjoint intervals, add the new interval to the list
-				newInterval = interval;
-				result.add(newInterval);
-			}
-		}
-
-		return result.toArray(new int[result.size()][]);
-	}
+            for else if result(new list) last node is greater then it means it can encapsulate the interval in itself but 
+            we have check the last element if it is less or gerater than the range of our existing new node.
+            eg [4,8] [3,7] ->3 is less than 4 but 7 is also coming under the range of new list (result)   
+            */
+            if(result.isEmpty()||result.get(result.size() - 1)[1]<interval[0]){
+                result.add(interval);
+            }
+            
+            else{
+                result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1],interval[1]);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
 }
